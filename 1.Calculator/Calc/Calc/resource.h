4b265fa7fb1888475fedd9c648754ffdcb353767
jwson <sjw.onenb@gmail.com>
2023-10-05 01:33:48 +0900

    1.Dialog UI 배치

---
+++ 1.Calculator/Calc/Calc/resource.h
@@ -0,0 +1,40 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Calc.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CALC_DIALOG                 102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1016
+#define IDC_BUTTON3                     1017
+#define IDC_BUTTON4                     1018
+#define IDC_BUTTON5                     1019
+#define IDC_BUTTON6                     1020
+#define IDC_BUTTON7                     1021
+#define IDC_BUTTON8                     1022
+#define IDC_BUTTON9                     1023
+#define IDC_BUTTON10                    1024
+#define IDC_BUTTON11                    1025
+#define IDC_BUTTON12                    1026
+#define IDC_BUTTON13                    1027
+#define IDC_BUTTON14                    1028
+#define IDC_BUTTON15                    1029
+#define IDC_BUTTON16                    1030
+#define IDC_BUTTON17                    1031
+#define IDC_BUTTON18                    1032
+#define IDC_BUTTON19                    1033
+#define IDC_BUTTON20                    1034
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
